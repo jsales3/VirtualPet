c0208250fbc0c64ef04499c8ce368f2053771958
jsales3 <jsales3>
2018-02-05 00:24:59 -0500

    Initial Commit

---
+++ VirtualPet/PetLion.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class PetLion
+    {
+        //fields
+        private string name;
+        private string furColor;
+        private int hungryLevel = 0;
+        private int thirstLevel = 0;
+        private int boredLevel = 0;
+        private int foodAmount = 0;
+        private int weight;
+        private bool isFull = false;
+        private string foodChoice;
+        
+
+        
+        //Properties
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string FurColor
+        {
+            get { return furColor; }
+            set { furColor = value; }
+        }
+
+        public int Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+
+        public int HungryLevel
+        {
+            get { return hungryLevel; }
+            
+        }
+
+        public int ThirstLevel
+        {
+            get { return thirstLevel; }
+           
+        }
+
+        public int BoredLevel
+        {
+            get { return boredLevel; }
+           
+        }
+
+        public int FoodAmount
+        {
+            get { return foodAmount; }
+
+        }
+
+        public string FoodChoice
+        {
+            get { return foodChoice; }
+            set { foodChoice = value; }
+        }
+
+        public bool IsFull
+        {
+            get { return isFull; }
+            set { isFull = value; }
+        }
+
+
+        //constructors
+        public PetLion()
+        {
+
+        }
+
+        public PetLion(string name, string furColor, int weight)
+        {
+            this.name = name;
+            this.furColor = furColor;
+            this.weight = weight;
+        }
+
+        public void Feed(string foodType)
+        {
+            switch (foodType)
+            {
+                case "beef":
+                    hungryLevel = 0;
+                    thirstLevel ++;
+                    foodAmount++;
+                    Console.WriteLine("you feed " + name + " beef");
+                    break;
+
+                case "pork":
+                    hungryLevel = 0 ;
+                    thirstLevel ++;
+                    foodAmount++;
+                    Console.WriteLine("you feed " + name + " pork");
+                    break;
+
+                case "chicken":
+                    hungryLevel = 0;
+                    thirstLevel++;
+                    foodAmount++;
+                    Console.WriteLine("you feed " + name + " chicken" );
+                    break;
+            }
+        }
+
+        public void GiveWater()
+        {
+            thirstLevel = 0;
+            Console.WriteLine("you gave " + name + " some water");
+
+        }
+
+        public void PlayWith()
+        {
+            boredLevel = 0;
+            Console.WriteLine(name + " had a nice walk");
+           
+        }
+
+        public void ToBathroom()
+        {
+            hungryLevel = 0;
+            thirstLevel = 0;
+            foodAmount = 0;
+            isFull = false;
+            Console.WriteLine(name + " has used the bathroom");
+        }
+
+        public void tick()
+        {
+                hungryLevel++;
+                thirstLevel++;
+                boredLevel++;
+        }
+
+        public void Food()
+        {
+            List<string> foodType = new List<string> { "Beef", "Pork", "Chicken" };
+
+            foreach(string food in foodType)
+            {
+                Console.WriteLine(food);
+                
+            }
+            
+        }
+
+
+
+        
+
+        
+        
+
+    }
+}
---
+++ VirtualPet/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            PetLion MyLion = new PetLion();
+            
+
+            Console.WriteLine("Hello!, let's create a virtual pet Lion!");
+            Console.WriteLine("\nWhat would you like to name your pet?");
+            Console.WriteLine("Enter a name below");
+            MyLion.Name = Console.ReadLine();
+
+            Console.WriteLine("\nNow let's choose a fur color for your pet Lion:");
+
+            List<string> petColor = new List<string> { "Brown", "Tan", "Grey", "Black" };
+
+            foreach(string color in petColor)
+            {
+                Console.WriteLine(color);
+            }
+
+            Console.WriteLine("\nSelect and enter a color from the list above" );
+
+            MyLion.FurColor = Console.ReadLine();
+
+            Console.WriteLine("\nGreat, now we can give your pet a weight!");
+            Console.WriteLine("\nHow much would you like your pet to weigh in pounds?");
+
+            MyLion.Weight = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nCongrats!, you have sussefully created your virtual pet Lion.\nnow lets get started!");
+            Console.WriteLine("\nHere are your current pet details:");
+            Console.WriteLine("Pet Name: " + MyLion.Name + "\nFur Color: " + MyLion.FurColor + "\nWeight: "
+                + MyLion.Weight + "\nHunger Level: " + MyLion.HungryLevel + "\nThirst Level: " + MyLion.ThirstLevel +
+                "\nBored Level: " + MyLion.BoredLevel);
+            int userAction;
+
+            do
+            {
+               
+                Console.WriteLine("\nWhat would you like to do?, Select a number that corresponds to an action");
+                Console.WriteLine("1. Feed Pet \n2. Give Water \n3. Play with Pet \n4. Take to Bathroom \n5. See Pet Details \n6. Quit Program" );
+                MyLion.tick();
+
+                userAction = int.Parse(Console.ReadLine());
+
+                switch (userAction)
+                {
+                    case 1:
+                        if (MyLion.IsFull == true)
+                        {
+                            Console.WriteLine(MyLion.Name + " is full");
+                        }
+                        else
+                        {
+                            Console.WriteLine("What would you like to feed your pet?");
+                            MyLion.Food();
+                            MyLion.FoodChoice = Console.ReadLine().ToLower();
+                            MyLion.Feed(MyLion.FoodChoice);
+                        }
+                        break;
+
+                    case 2:
+                        MyLion.GiveWater();
+                        break;
+
+                    case 3:
+                        MyLion.PlayWith();
+                        break;
+
+                    case 4:
+                        MyLion.ToBathroom();
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Pet Name: " + MyLion.Name + "\nFur Color: " + MyLion.FurColor + "\nWeight: "
+               + MyLion.Weight + "\nHunger Level: " + MyLion.HungryLevel + "\nThirst Level: " + MyLion.ThirstLevel +
+               "\nBored Level: " + MyLion.BoredLevel);
+                        break;
+
+                    default:
+                        Console.WriteLine("Select a number that corresponds to an action");
+                        break;
+                }
+
+                if (MyLion.HungryLevel == 4)
+                {
+                    Console.WriteLine(MyLion.Name + " is hungry!");
+                   
+
+                }
+                else if (MyLion.ThirstLevel == 3)
+                {
+                    Console.WriteLine(MyLion.Name + " is getting thirsty!");
+                   
+
+                }
+                else if (MyLion.BoredLevel == 2 || MyLion.BoredLevel == 6)
+                {
+                    Console.WriteLine(MyLion.Name + " is bored");
+                   
+                   
+                }
+                if(MyLion.FoodAmount == 2)
+                {
+                    MyLion.IsFull = true;
+                }
+
+                if ((MyLion.IsFull == true) || (MyLion.ThirstLevel  == 10))
+                {
+                    Console.WriteLine(MyLion.Name + " needs to use the bathroom!");
+                    
+                }
+
+            }
+            while (userAction != 6);
+
+
+          
+                
+          
+        }
+    }
+}
